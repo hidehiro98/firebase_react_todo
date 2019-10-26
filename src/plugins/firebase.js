8ec07912556b634e939b95c735c06b2337d451cd
acorn181 <masa.ichi.181@gmail.com>
2019-10-26 16:17:47 +0900

    firebase用の設定を追加

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyDQkm8mkAYb2bAEEN0al-1ImI0hzEbIKhQ",
-    authDomain: "react-nagaoka-20191013.firebaseapp.com",
-    databaseURL: "https://react-nagaoka-20191013.firebaseio.com",
-    projectId: "react-nagaoka-20191013",
-    storageBucket: "react-nagaoka-20191013.appspot.com",
-    messagingSenderId: "879652289929",
-    appId: "1:879652289929:web:e4e9bc3bd65163bc4bb558",
-    measurementId: "G-J2B4BSSHTD"
+    apiKey: "AIzaSyDIWnbJ4Z4EX9dZpkgAH-pAXM0LbSe_jqM",
+    authDomain: "sample-masahich.firebaseapp.com",
+    databaseURL: "https://sample-masahich.firebaseio.com",
+    projectId: "sample-masahich",
+    storageBucket: "sample-masahich.appspot.com",
+    messagingSenderId: "922515761638",
+    appId: "1:922515761638:web:e8c5e293ad2a6f896e8e27",
+    measurementId: "G-1L92SGZSWJ"
 };
 const firebaseApp = firebase.initializeApp(config);
 export const firestore = firebaseApp.firestore();
